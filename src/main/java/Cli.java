6b47ac9790546746752de4c17f36cee3df715206
dmozh99 <dmozh99@mail.ru>
2020-04-05 20:55:55 +0300

    CLI added;

---
+++ src/main/java/Cli.java
@@ -0,0 +1,23 @@
+import exception.SqlParsingException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Cli {
+    public static void main(String[] args) throws IOException {
+        Sql2MongoTranslator translator = new Sql2MongoTranslator();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Enter SELECT query if you want to translate it to MongoDB query.");
+        System.out.println("Enter 'q' if you want to exit.");
+        String line;
+        while (!"q".equals(line = reader.readLine()) && line != null) {
+            try {
+                System.out.println(translator.select2Find(line));
+            } catch (SqlParsingException e) {
+                System.err.println(e.getMessage());
+            }
+        }
+    }
+}
